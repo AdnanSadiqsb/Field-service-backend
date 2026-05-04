8df57bc941d16b9f3df4f6d964693976edf58c68
adnan@earnflex.com <adnan@earnflex.com>
2026-05-05 00:12:27 +0500

    imports fixes

--- src/professionals/urls.py
+++ src/professionals/urls.py
@@ -6,4 +6,5 @@ router.register(r'trades', TradeCategoryViewSet)
 router.register(r'profiles', ProfessionalProfileViewSet)
 router.register(r'coverage-areas', ProfessionalCoverageAreaViewSet)
 
+professionals_router = router
 urlpatterns = router.urls
